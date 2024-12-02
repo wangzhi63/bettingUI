3b6190743da6861e7c855e41f77b17b0f87467e3
zhi wang <wangzhi_63@yahoo.com>
2024-12-02 14:39:13 -0500

    make the login work by fixing the problem of timing of token availability

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,59 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { OAuthService } from 'angular-oauth2-oidc';
+
+// @Component({
+//   selector: 'app-home',
+//   template: `
+//     <div *ngIf="!isAuthenticated">Loading...Not authenticated yet, you might need to hit the enter again</div>
+//     <div *ngIf="isAuthenticated">
+//       <h1>Welcome, {{ user?.name }}</h1>
+//       <h1>we will notify you through this email {{ user?.email }}</h1>
+//       <button (click)="logout()">Logout</button>
+//     </div>
+//   `
+// })
+// export class HomeComponent {
+//  isAuthenticated: boolean = false;
+//   user: any;
+
+//   constructor(private oauthService: OAuthService) {
+//     this.isAuthenticated = this.oauthService.hasValidAccessToken();
+//     if (this.isAuthenticated) {
+//       this.user = this.oauthService.getIdentityClaims();
+//       console.log(this.user);
+//     }
+//   }
+
+//   logout() {
+//     this.oauthService.logOut();
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <div *ngIf="!isAuthenticated">Loading...</div>
-    <div *ngIf="isAuthenticated">
-      <h1>Welcome, {{ user?.name }}</h1>
-      <h1>we will notify you through this email {{ user?.email }}</h1>
-      <button (click)="logout()">Logout</button>
-    </div>
-  `
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
- isAuthenticated: boolean = false;
+export class HomeComponent implements OnInit {
+  isAuthenticated: boolean = false;
   user: any;
 
-  constructor(private oauthService: OAuthService) {
-    this.isAuthenticated = this.oauthService.hasValidAccessToken();
-    if (this.isAuthenticated) {
-      this.user = this.oauthService.getIdentityClaims();
-      console.log(this.user);
-    }
-  }
+  constructor(private oauthService: OAuthService) {}
 
-  logout() {
-    this.oauthService.logOut();
+  ngOnInit(): void {
+    // Check the initial authentication state
+    this.isAuthenticated = this.oauthService.hasValidAccessToken();
+    this.user = this.oauthService.getIdentityClaims();
+
+    // Subscribe to authentication state changes
+    this.oauthService.events.subscribe(event => {
+      if (event.type === 'token_received' || event.type === 'token_refreshed') {
+        this.isAuthenticated = this.oauthService.hasValidAccessToken();
+        this.user = this.oauthService.getIdentityClaims();
+      }
+    });
   }
 }
